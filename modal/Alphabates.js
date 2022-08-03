46e2dde6133e7f66bbb8c3068dd76f5c6a7ab0b4
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2022-08-03 08:18:13 +0530

    Add complete app

--- modal/Alphabates.js
+++ modal/Alphabates.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const AlphabatSchema = new mongoose.Schema({
-   A : { type : Number },
-   B : { type : Number },
-   C : { type : Number },
-   D : { type : Number },
-   E : { type : Number },
-   F : { type : Number },
+    name : {
+       char : {
+         type : String
+       },
+       value : {
+         type : Number
+       }
+    }
 });
 
 const Alpha = mongoose.model('Alpha', AlphabatSchema);
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 require('dotenv').config({path : "./.env"});
 const port = parseInt(process.env.PORT) || 8000;
 const connectDb = require('./config/db');
+const cors = require('cors');
 
 // db configuration
 connectDb();
 
+app.use(cors());
+
 // routes configure
 app.use('/' , require('./routes/index'));
+
 
 app.listen(port, () => {
     console.log("server is running on port " , port);
